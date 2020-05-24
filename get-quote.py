c5cc4abf18a3e765095d803eb6e74a5868a2ad5d
Avinash Anand <36325275+anand-avinash@users.noreply.github.com>
2020-05-24 17:06:22 +0530

    Done

--- get-quote.py
+++ get-quote.py
@@ -1,15 +1,15 @@
 import random
 
 def gain():
-  print("Keep it logically awesome.")
-
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
   last = len(quotes)-1
-  rnd = random.randint(0, last)
-  print(quotes[rnd])
+  rnd1 = random.randint(0, last)
+  rnd2 = random.randint(0, last)
+
+  print(f"{quotes[rnd1]}{quotes[rnd2]}", end='')
 
 if __name__== "__main__":
   gain()
